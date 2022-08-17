ce5276dbf887325144decf0c97836e0b061c45eb
Francesco Antonetti Lamorgese Passeri <francescoantonetti1@gmail.com>
2022-08-17 13:25:39 +0200

    -

--- window.cpp
+++ window.cpp
@@ -14,6 +14,8 @@
 
 #include "Arduino.h"
 
+#define DEBUG
+
 window::window(UTFT& tft,int* size,int* position) : tft{tft},position{position[0],position[1]},size{size[0],size[1]},fontSize{tft.getFontXsize(),tft.getFontYsize()}
 {
 	this->drawBorders();
@@ -52,7 +54,10 @@ void window::text(String text)
 	int maxLineChars = this->size[0]/fontSizeX;
 
 	textLength = this->tft.getFontXsize()*text.length();
-
+#ifdef DEBUG
+	Serial.print("Text length: ");
+	Serial.println(text.length());
+#endif
 	if((textLength < this->size[0]) && (text.indexOf("\n") < 0))
 	{
 		textPositionX = (this->position[0]+(this->size[0]-textLength)/2);
@@ -63,34 +68,39 @@ void window::text(String text)
 	{
 		const int numLines = (this->size[1]-2)/this->tft.getFontYsize();
 		String lines[numLines];
-		for(int i=0,l=0,j=((textLength > maxLineChars) ? maxLineChars : textLength);(j<=textLength) && (l<numLines);++l)
+
+		for(int i=0,l=0;l < numLines;++l)
 		{
+			String line = text.substring(i,(i+maxLineChars < text.length()) ? i+maxLineChars : i+text.length());
+			int newLine = line.indexOf("\n");
 #ifdef DEBUG
 			Serial.print("i: ");
 			Serial.println(i);
-			Serial.print("j: ");
-			Serial.println(j);
+			Serial.print("line: ");
+			Serial.println(line);
 #endif
-			int newLine = text.indexOf("\n");
-			String line = text.substring(i,j);
-			if((newLine >= 0) && (newLine <= j))
+			if((newLine >= 0) && (newLine <= text.length()))
 			{
 				text.remove(text.indexOf("\n"),1);
 				line = text.substring(i,newLine);
 				line.remove(line.indexOf("\n"),1);
-				i = newLine;
 #ifdef DEBUG
+				Serial.print("Text length: ");
+				Serial.println(text.length());
 				Serial.print("text: ");
 				Serial.println(text);
 				Serial.print("line: ");
 				Serial.println(line);
+				Serial.println();
 #endif
-				lines[l] = line;
-				if((i == j) && (j < textLength))
-				{
-					i = j;
-					((j+maxLineChars) < textLength) ?  j += maxLineChars : j = textLength;
-				}
+				if(line.length())
+					lines[l] = line;
+				else
+					--l;
+				if(i < text.length())
+					i += newLine;
+				else
+					break;
 				continue;
 			}
 #ifdef DEBUG
@@ -98,11 +108,13 @@ void window::text(String text)
 			Serial.println(text);
 			Serial.print("line: ");
 			Serial.println(line);
+			Serial.println();
 #endif
 			lines[l] = line;
-			i = j;
-			int tmp = j+maxLineChars;
-			(tmp < textLength) ? j+=maxLineChars : j=textLength;
+			if(i < text.length())
+				(i+maxLineChars > text.length()) ? i += maxLineChars : i += text.length();
+			else
+				break;
 		}
 		for(int i=0;i<numLines;i++)
 		{
